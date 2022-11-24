4288d7345d120c32ae6636ae14930ee2a03e2d57
uzi <rytuiytyuttrye567@gmail.com>
2022-11-24 19:30:54 +0500

    upload

--- src/components/UploadFile.js
+++ src/components/UploadFile.js
@@ -9,37 +9,37 @@ function UploadFile({ props, files, name, filesUploaded, setFileUploaded }) {
     formData.append("name_" + i, v);
   });
   formData.append("upload_Files", true);
-
-  const Upload = axios.post(
-    "https://cricshizz.com.pk/bucket/index.php",
-    formData,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    }
-  );
-  Upload.then((r) => {
-    const Result = r.data;
-    if (Result?.success) {
-      if (setFileUploaded) {
-        var tmp_array = { ...filesUploaded };
-        tmp_array.success.push(1);
-        setFileUploaded({ ...tmp_array });
+  window.$.ajax({
+    url: "http://bucket.cricshizz.com.pk:8080/api/upload_file",
+    data: formData,
+    type: "POST",
+    processData: false,
+    crossDomain: true,
+    contentType: false,
+    success: function (data) {
+      const Result = data;
+      console.log(Result);
+      if (Result?.success) {
+        if (setFileUploaded) {
+          var tmp_array = { ...filesUploaded };
+          tmp_array.success.push(1);
+          setFileUploaded({ ...tmp_array });
+        }
+      } else {
+        if (setFileUploaded) {
+          var tmp_array = { ...filesUploaded };
+          tmp_array.failed.push(1);
+          setFileUploaded({ ...tmp_array });
+        }
       }
-    } else {
+    },
+    error: function () {
       if (setFileUploaded) {
         var tmp_array = { ...filesUploaded };
         tmp_array.failed.push(1);
         setFileUploaded({ ...tmp_array });
       }
-    }
-  }).catch((err) => {
-    if (setFileUploaded) {
-      var tmp_array = { ...filesUploaded };
-      tmp_array.failed.push(1);
-      setFileUploaded({ ...tmp_array });
-    }
+    },
   });
 }
 export default UploadFile;
